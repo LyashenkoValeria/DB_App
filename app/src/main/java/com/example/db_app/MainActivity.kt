65c6530711e2f0fae16b7e798eb952de76767873
LyashenkoValeria <leovaldez00@mail.ru>
2021-05-13 01:02:37 +0300

    Сделан макет профиля/редактора профиля, закончены главный макет и любимые жанры

--- app/src/main/java/com/example/db_app/MainActivity.kt
+++ app/src/main/java/com/example/db_app/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.db_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.profile_edit)
+    }
 
-
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.logout, menu)
+        return true
     }
 }
